f9081bc05cbaa16d238c8ebe178bae4c056e8893
ssv1982 <ssv1982@gmail.com>
2015-06-19 15:17:00 +0700

    уборка мусора

--- bareos.pl
+++ bareos.pl
@@ -3,27 +3,22 @@
 $bareosDbUser='bareos';
 $bareosDbName='bareos';
 
-#@qq=`/usr/bin/psql -U$User_Preferences{'bareosDbUser'} -d$User_Preferences{'bareosDbName'} -t -A -c "select job.name,client.name from job left join client on job.clientid=client.clientid group by job.name,client.name;"`;
-@qq=`/usr/bin/psql -U$bareosDbUser -d$bareosDbName -t -A -c "select job.name,client.name from job left join client on job.clientid=client.clientid group by job.name,client.name having client.name='$ARGV[0]';"`;
+@jobs=`/usr/bin/psql -U$bareosDbUser -d$bareosDbName -t -A -c "select job.name,client.name from job left join client on job.clientid=client.clientid group by job.name,client.name having client.name='$ARGV[0]';"`;
 
 $first = 1;
   
 print "{\n";
 print "\t\"data\":[\n\n";
-   
-foreach $arg(@qq)
+foreach $arg(@jobs)
 {
   $arg=~s/\n//; 
   @jobs=split(/\|/, $arg);
-
   print "\t,\n" if not $first;
 	$first = 0;
-	
 	print "\t{\n";
   print "\t\t\"{#JOBNAME}\":\"$jobs[0]\",\n";
   print "\t\t\"{#CLIENTNAME}\":\"$jobs[1]\"\n";
   print "\t}\n";
-        
 }
 print "\n\t]\n";
 print "}\n";
